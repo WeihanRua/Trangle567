394c6380aceda99a887bbec8d14dd5bc29f46ecd
Weihan Xu <wxu19960506@gmail.com>
2019-03-05 12:12:21 -0500

    little change about static code
    fixed some code problem with static analyzer tool

--- Triangle.py
+++ Triangle.py
@@ -3,13 +3,14 @@
 Created on Thu Jan 14 13:44:00 2016
 Updated Jan 21, 2018
 
-The primary goal of this file is to demonstrate a simple python program to classify triangles
+a simple python program to classify triangles
 
 @author: jrr
 @author: rk
 """
 
-def classifyTriangle(a,b,c):
+
+def classifyTriangle(a, b, c):
     # change nothing
     if a > 200 or b > 200 or c > 200:
         return 'InvalidInput'
@@ -17,7 +18,7 @@ def classifyTriangle(a,b,c):
     if a <= 0 or b <= 0 or c <= 0:
         return 'InvalidInput'
     # change nothing
-    if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
+    if not(isinstance(a, int) and isinstance(b, int) and isinstance(c, int)):
         return 'InvalidInput'
     # replace '-' by '+' 
     if (a >= (b + c)) or (b >= (a + c)) or (c >= (a + b)):
@@ -26,9 +27,10 @@ def classifyTriangle(a,b,c):
     # change a lot
     elif a == b and b == c:
         return 'Equilateral'
-    elif ((a * a) + (b * b)) == (c * c) or ((a * a) + (c * c)) == (b * b) or ((b * b) + (c * c)) == (a * a):
+    elif ((a * a) + (b * b)) == (c * c) or ((a * a) + (c * c)) == (b * b) or\
+         ((b * b) + (c * c)) == (a * a):
         return 'Right'
-    elif (a != b) and  (b != c) and (a != c):
+    elif (a != b) and (b != c) and (a != c):
         return 'Scalene'
     else:
         return 'Isoceles'
